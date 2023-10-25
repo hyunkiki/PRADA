310fe96bf701843b362a596a39409d60086234cc
김현기 <rlagusrl98@gmail.com>
2023-10-25 18:14:33 +0900

    10.25 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -61,20 +61,26 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-var acc = document.getElementsByClassName("header_accordion");
+var accHeader = document.getElementsByClassName("header_accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
+for (i = 0; i < accHeader.length; i++) {
+  accHeader[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + 200 + "px";
     }
   });
 }
+// $(function () {
+//   $(".header_accordion").click(function () {
+//     $(".panel").removeClass("active");
+//     $(this).addClass("active");
+//   });
+// });
 
 var swiper = new Swiper(".mySwiper_main", {
   slidesPerView: "auto",
