2678c26ace2b13abcc6a62d436d8cb541296dc02
김현기 <rlagusrl98@gmail.com>
2023-11-01 18:16:11 +0900

    11.01 업데이트

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -78,20 +78,34 @@ $(function () {
   });
 });
 
-// var acc = document.getElementsByClassName("accordion");
-// var i;
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function () {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     }
-//   });
-// }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+var accitm = document.getElementsByClassName("accordionItem");
+var i;
+
+for (i = 0; i < accitm.length; i++) {
+  accitm[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
 
 // var accHeader = document.getElementsByClassName("header_accordion");
 // var i;
